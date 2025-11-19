a50b8dcee79175db1462e2f1b66a0dbb9eeb3fa5
JLYugsi <jlyugsi@espe.edu.ec>
2025-11-18 19:11:04 -0500

    Actualizacion de Calculadora

--- PRY_CALCULADORA/public/js/main.js
+++ PRY_CALCULADORA/public/js/main.js
@@ -2,7 +2,6 @@ import { BotonOperacion } from './BotonOperacion.js';
 import { Calculadora } from './Calculadora.js';
 import { BotonNumero } from './BotonNumero.js';
 
-
 customElements.define('boton-operacion', BotonOperacion);
 customElements.define('calculadora-basica', Calculadora);
 customElements.define('boton-numero', BotonNumero);
